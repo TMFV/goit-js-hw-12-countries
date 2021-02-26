d2fa0d8629f7104bf4a76d96bd14b947aa4127a7
TMFV <68297974+TMFV@users.noreply.github.com>
2021-02-26 22:55:47 +0200

    1
    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import './styles.css';
+//import fetchCountries from './js/fetchCountries';
+import fetch from 'cross-fetch';
+fetch('https://restcountries.eu/rest/v2/all')
+.then(response => {
+    console.log(response.json());
+  })
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,4 @@
+//export default function fetchCountries(searchQuery) {
+   // fetch('https://restcountries.eu/rest/v2/name/')
+//  console.log(searchQuery);  
+//};
